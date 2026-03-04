82e0df783e7e1fc76a49150b612535455d363c5c
AkilanSS <akilanss22@gmail.com>
2026-03-04 21:55:46 +0530

    fix types

--- src/config.zig
+++ src/config.zig
@@ -1,5 +1,5 @@
-pub const GRID_SIZE: u16 = 100;
+pub var GRID_SIZE: f32 = 75;
 pub const N: u16 = 7;
-pub const WINDOW_HEIGHT: u16 = 1000;
-pub const WINDOW_WIDTH: u16 = 1000;
-pub const WINDOW_LAYOUT_HEIGHT: u16 = 800;
+pub const WINDOW_HEIGHT: f32 = 1000;
+pub const WINDOW_WIDTH: f32 = 1000;
+pub const WINDOW_LAYOUT_HEIGHT: f32 = 800;
--- src/main.zig
+++ src/main.zig
@@ -18,16 +18,21 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
 
         //Below code will draw the grid, based on config
-        const x_center = cfg.WINDOW_WIDTH / 2;
-        const y_center = cfg.WINDOW_LAYOUT_HEIGHT / 2;
-        const grid_x_corner = x_center - cfg.GRID_SIZE * cfg.N / 2;
-        const grid_y_corner = y_center - cfg.GRID_SIZE * cfg.N / 2;
-        rl.drawRectangleV(rl.Vector2{ .x = grid_x_corner, .y = grid_y_corner }, rl.Vector2{ .x = cfg.GRID_SIZE * cfg.N, .y = cfg.GRID_SIZE * cfg.N }, .black);
+        const total_size = cfg.GRID_SIZE * @as(f32, @floatFromInt(cfg.N));
+        const x_center = cfg.WINDOW_WIDTH / 2.0;
+        const y_center = cfg.WINDOW_LAYOUT_HEIGHT / 2.0;
+        const grid_x_corner: f32 = x_center - cfg.GRID_SIZE * cfg.N / 2.0;
+        const grid_y_corner: f32 = y_center - cfg.GRID_SIZE * cfg.N / 2.0;
+
+        rl.drawRectangleV(rl.Vector2{ .x = grid_x_corner, .y = grid_y_corner }, rl.Vector2{ .x = total_size, .y = total_size }, .black);
 
         for (0..cfg.N) |i| {
-            rl.drawLineEx(rl.Vector2{ .x = @floatFromInt(cfg.GRID_SIZE * i + grid_x_corner), .y = grid_y_corner }, rl.Vector2{ .x = @floatFromInt(cfg.GRID_SIZE * i + grid_x_corner), .y = @floatFromInt(grid_y_corner + cfg.N * cfg.GRID_SIZE) }, 1.0, .light_gray);
-            rl.drawLineEx(rl.Vector2{ .y = @floatFromInt(cfg.GRID_SIZE * i + grid_y_corner), .x = grid_x_corner }, rl.Vector2{ .y = @floatFromInt(cfg.GRID_SIZE * i + grid_y_corner), .x = @floatFromInt(grid_x_corner + cfg.N * cfg.GRID_SIZE) }, 1.0, .light_gray);
+            const offset = @as(f32, @floatFromInt(i)) * cfg.GRID_SIZE;
+            rl.drawLineEx(.{ .x = grid_x_corner + offset, .y = grid_y_corner }, .{ .x = grid_x_corner + offset, .y = grid_y_corner + total_size }, 1.0, .light_gray);
+            rl.drawLineEx(.{ .x = grid_x_corner, .y = grid_y_corner + offset }, .{ .x = grid_x_corner + total_size, .y = grid_y_corner + offset }, 1.0, .light_gray);
         }
+        //Render logic for displaying the grid ends here
+
         rl.clearBackground(.white);
     }
 }
